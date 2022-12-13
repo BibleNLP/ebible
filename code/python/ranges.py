83e46c3511b1d08d60258538ca7f3d7d73ec16b9
David Baines <david_baines@sil.org>
2022-12-13 14:28:43 +0000

    Update ranges.py to report short extracts.

--- code/python/ranges.py
+++ code/python/ranges.py
@@ -1,65 +1,64 @@
+"""ranges.py tidies up files extracted by SIL Machine by removing empty verse ranges.
+The fix_file function iterates though a file and removes lines that consist of only '<range>' or '...' where the line before is  empty. """
+
 import argparse
-#from itertools import chain
 import multiprocessing as mp
 import os
 from pathlib import Path
 
 
 def fix_file(params):
-    
+
     file, output_folder = params
 
     range_count = 0
     verse_count = 0
     empty_range = 0
 
-    with open(file, 'r', encoding='utf-8') as f_in:
+    deletable_lines = set(["<range>", "..."])
+
+    with open(file, "r", encoding="utf-8") as f_in:
         lines = f_in.readlines()
 
-    for idx in range(1, len(lines)-1):
-        
-        prev_line = lines[idx-1].strip()
+    for idx in range(1, len(lines) - 1):
+
+        prev_line = lines[idx - 1].strip()
         line = lines[idx].strip()
 
         # Count verses with some text, including range markers.
-        if  line != '':
+        if line != "":
             verse_count += 1
-            if line in ["<range>", "..."]:
+            if line in deletable_lines:
                 range_count += 1
-                if prev_line == '':
+                if prev_line == "":
                     empty_range += 1
                     verse_count -= 1
-                    lines[idx] = ''
+                    lines[idx] = ""
 
-        #print(range_count, lines[idx-1],lines[idx])
+        # print(range_count, lines[idx-1],lines[idx])
 
-
-    if verse_count > 0 and empty_range > 0 :
+    if verse_count > 0 and empty_range > 0:
         output_file = output_folder / file.name
 
-        with open(output_file, 'w', encoding='utf-8',newline='\n') as f_out:
+        with open(output_file, "w", encoding="utf-8", newline="\n") as f_out:
             f_out.writelines(lines)
-    
+
     return (verse_count, range_count, empty_range)
 
 
 def main():
     parser = argparse.ArgumentParser(description="Removes empty ranges from extracts.")
     parser.add_argument("input", type=Path, help="The extract folder.")
-    parser.add_argument("output", type=Path, help="The folder for modified output files.")
+    parser.add_argument(
+        "output", type=Path, help="The folder for modified output files."
+    )
     args = parser.parse_args()
 
     input = Path(args.input)
-    
     output_folder = Path(args.output)
-    
     files = [file for file in input.glob("*.txt")]
-
-    #for file in files[:10]:
-    #    range_count, verse_count = fix_file(file)
-    #    print(verse_count,range_count,file)
-
-    #exit()
+    empty_files = list()
+    short_files = list()
 
     # Set processors to use
     cpus_to_use = 20
@@ -72,32 +71,59 @@ def main():
 
     total_verses = 0
     total_ranges = 0
-    total_empty  = 0
-    files_with_empty = 0
+    total_empty = 0
+    files_with_empty_ranges = list()
 
-    for file, counts in zip(files,counts):
+    for file, counts in zip(files, counts):
         verse_count, range_count, empty_range = counts
         total_verses += verse_count
         total_ranges += range_count
-        total_empty  += empty_range
+        total_empty += empty_range
         if empty_range > 0:
-            files_with_empty += 1
+            files_with_empty_ranges.append(file)
+
         if verse_count == 0:
-            os.remove(file) 
-            if os.path.exists(file):
-                print(f"File {file} contained no verse data but the attempt to delete it failed.")
-            else:
-                print(f"File {file} contained no verse data and was deleted.")
+            files_to_delete = [file, output_folder / file.name]
+            for file_to_delete in files_to_delete:
+                if os.path.exists(file_to_delete):
+                    os.remove(file_to_delete)
+                    if os.path.exists(file_to_delete):
+                        print(
+                            f"Attempted to delete empty file: {file_to_delete} but the attempt failed."
+                        )
+                    else:
+                        print(f"Deleted empty file: {file_to_delete}")
+
+        if verse_count < 200 :
+            short_files.append(file)
 
 
-        #files[file] = {"verse_count" :verse_count, "range_count": range_count,"empty_range":empty_range}
+        # files[file] = {"verse_count" :verse_count, "range_count": range_count,"empty_range":empty_range}
 
-        print(counts,file)
+        # print(counts,file)
 
-    print(f"There are {len(files)} files, of which {files_with_empty} have empty ranges.")
+    print(f"There are {len(files)} files.")
+
+    if len(files_with_empty_ranges) > 0:
+        print(f"\nThese {len(files_with_empty_ranges)} files had empty ranges which were removed:")
+        for file_with_empty_ranges in files_with_empty_ranges:
+            print(file_with_empty_ranges)
+        print()
+    
+    if len(short_files) > 0:
+        print(f"\nThese {len(short_files)} have fewer than 200 verses.")
+        for short_file in short_files:
+            print(short_file)
+        print()
+    
+
     print(f"There are a total of {total_verses} verses.")
-    print(f"There are a total of {total_ranges} ranges. {total_ranges/total_verses * 100:.3f}% of verses are ranges")
-    print(f"There are a total of {total_empty} empty ranges. {total_empty/total_verses * 100:.4f}% of verses are empty_ranges")
+    print(
+        f"There are a total of {total_ranges} ranges. {total_ranges/total_verses * 100:.3f}% of verses are ranges"
+    )
+    print(
+        f"There are a total of {total_empty} empty ranges. {total_empty/total_verses * 100:.4f}% of verses are empty_ranges"
+    )
     print(f"{total_empty/total_ranges * 100:.4f}% of ranges are empty_ranges.")
 
 
