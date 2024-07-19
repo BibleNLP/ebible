da0a9e8f56fe3804c81f72a291d535b12fd4b2f8
HARMONSC21 <harmonsc21@gcc.edu>
2024-07-19 10:44:25 -0400

    Reverted ebible.py and updated settings_file.py

--- code/python/settings_file.py
+++ code/python/settings_file.py
@@ -1,10 +1,31 @@
 import codecs
-from datetime import datetime
 import re
+import shutil
+from datetime import datetime
 from glob import iglob
 from os import listdir
 from pathlib import Path
 from typing import List, Tuple
+
+import yaml
+from machine.corpora import ParatextTextCorpus, extract_scripture_corpus
+from machine.scripture.verse_ref import VerseRef
+
+versification_to_num: dict[str, int] = {
+    "Original": 1,
+    "Septuagint": 2,
+    "Vulgate": 3,
+    "English": 4,
+    "Russian Protestant": 5,
+    "Russian Orthodox": 6,
+}
+
+BOOK_NUM = r"[0-9].\-"
+
+EXCLUDE_ALPHANUMERICS = r"[^\w]"
+
+POST_PART = r"[a-z].+"
+
 
 # Should not need to duplicate this function in ebible.py and here.
 def log_and_print(file, s, type="Info") -> None:
@@ -157,25 +178,186 @@ def add_settings_file(project_folder, language_code):
         settings.write(setting_file_stub)
 
 
+def get_vrs_diffs() -> dict[str, dict[int, dict[int, list[str]]]]:
+    """
+    gets the differences in versifications from vrs_diffs.yaml
+    return: the versification differences
+    """
+    with open("vrs_diffs.yaml", "r") as file:
+        try:
+            vrs_diffs = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return vrs_diffs
+
+
+def check_vref(
+    prev: VerseRef,
+    vrs_diffs: dict[str, dict[int, dict[int, list[str]]]],
+    versifications: list[str],
+    ruled_out: list[str],
+) -> tuple[list[str], list[str]]:
+    """
+    checks a verse reference to try to determine the versification
+    param prev: the verse reference to check
+    param vrs_diffs: the list of differences in the versifications
+    param versifications: the list of possible versifications
+    param ruled_out: the list of versifications that have been ruled out
+    return: the list of possible versifications and the list of versifications that have been ruled out
+    """
+    try:
+        curr = vrs_diffs[prev.book]["last_chapter"]
+        key = prev.chapter_num
+    except:
+        try:
+            curr = vrs_diffs[prev.book][prev.chapter_num]
+            key = prev.verse_num
+        except:
+            return versifications, ruled_out
+    try:
+        curr_versifications = curr[key].copy()
+    except:
+        return versifications, ruled_out
+    if len(curr_versifications) == 1:
+        return curr_versifications, ruled_out
+    for num, versifs in curr.items():
+        if num != key:
+            for versif in versifs:
+                if not versif in ruled_out:
+                    ruled_out.append(versif)
+    to_remove = []
+    for versif in curr_versifications:
+        if versif in ruled_out:
+            to_remove.append(versif)
+    for versif in to_remove:
+        curr_versifications.remove(versif)
+    if curr_versifications and len(curr_versifications) < len(versifications):
+        return curr_versifications, ruled_out
+    return versifications, ruled_out
+
+
+def get_book_names(project_folder: Path) -> list[str]:
+    """
+    gets the book names from the specified bible
+    param lang_code: the language code of the bible
+    param include_file_names: indicates whether to include file names in the list or not
+    param projects: the projects folder
+    return: a list of book names with their corresponding file names
+    """
+    names = []
+    books = (book for book in listdir(project_folder) if ".usfm" in book)
+    for book in books:
+        name = re.sub(BOOK_NUM, "", book)
+        name = re.sub(EXCLUDE_ALPHANUMERICS, "", name)
+        name = re.sub(POST_PART, "", name)
+        names.append((name, book))
+    return names
+
+
+def write_temp_settings_file(project_folder: Path) -> None:
+    """
+    writes a temporary settings file as a place holder to be able to determine the versification
+    param lang_code: the language code of the bible
+    param projects: the projects folder
+    """
+    if not "Settings.xml" in listdir(project_folder):
+        with open(project_folder / "Settings.xml", "w", encoding="utf-8") as set_file:
+            lang_code = str(project_folder).split("\\")[-1]
+            set_file.write(
+                f"""<ScriptureText>
+                <BiblicalTermsListSetting>Major::BiblicalTerms.xml</BiblicalTermsListSetting>
+                <Naming BookNameForm="46-MAT" PostPart="{lang_code}.usfm" PrePart="" />
+                </ScriptureText>"""
+            )
+
+
+def get_corpus(
+    project_folder: Path, vrs_diffs: dict[str, dict[int, dict[int, list[str]]]]
+) -> list[tuple[str, VerseRef, VerseRef]]:
+    """
+    creates a corpus of books found in vrs_diffs
+    param lang_code: the language code of the bible
+    param vrs_diffs: the list of differences in the versifications
+    param projects: the projects folder
+    return: the corpus from the available books in the specified bible
+    """
+    vrs_path = project_folder / "versification"
+    vrs_path.mkdir(parents=True, exist_ok=True)
+    book_names = get_book_names(project_folder)
+    for name in book_names:
+        if name[0] in vrs_diffs.keys():
+            shutil.copyfile(project_folder / name[1], vrs_path / name[1])
+    write_temp_settings_file(project_folder)
+    shutil.copyfile(project_folder / "Settings.xml", vrs_path / "Settings.xml")
+    corpus = ParatextTextCorpus(vrs_path)
+    lines = list(extract_scripture_corpus(corpus, corpus))
+    shutil.rmtree(vrs_path)
+    return lines
+
+
+def get_versification(
+    project_folder: Path,
+    vrs_diffs: dict[str, dict[int, dict[int, list[str]]]],
+) -> tuple[str, list[str]]:
+    """
+    gets the versification of the given bible
+    param lang_code: the language code of the bible
+    param vrs_diffs: the list of differences in the versifications
+    param log: the list of information about downloads
+    param projects: the projects folder
+    return: the versification of the given bible
+    """
+    lines = get_corpus(project_folder, vrs_diffs)
+    versifications = [
+        "English",
+        "Original",
+        "Russian Protestant",
+        "Russian Orthodox",
+        "Septuagint",
+        "Vulgate",
+    ]
+    ruled_out = []
+    try:
+        prev = lines[0][1]
+    except:
+        return versifications[0]
+    for line in lines[1:]:
+        vref = line[1]
+        try:
+            vrs_diffs[prev.book]
+        except:
+            prev = vref
+            continue
+        if vref.chapter_num != prev.chapter_num:
+            versifications, ruled_out = check_vref(
+                prev, vrs_diffs, versifications, ruled_out
+            )
+            if len(versifications) == 1:
+                return versifications[0]
+        prev = vref
+    versifications, ruled_out = check_vref(prev, vrs_diffs, versifications, ruled_out)
+    return versifications[0]
+
+
 def write_settings_file(project_folder: Path):
 
     # Now add a Settings.xml file to a project folder.
     if project_folder.is_dir():
         language_code = str(project_folder.name)[:3]
         settings_file = project_folder / "Settings.xml"
-        
+
         if settings_file.is_file():
             return None
         else:
             # print(f"Adding Settings.xml to {project_folder}")
-            versification = get_versification(project_folder)
+            versification = get_versification(project_folder, get_vrs_diffs())
+            vrs_num = versification_to_num[versification]
             setting_file_text = f"""<ScriptureText>
-                                <Versification>{versification}</Versification>
+                                <Versification>{vrs_num}</Versification>
                                 <LanguageIsoCode>{language_code}:::</LanguageIsoCode>
                                 <Naming BookNameForm="41-MAT" PostPart="{project_folder.name}.usfm" PrePart="" />
                                 </ScriptureText>"""
 
-    
             with open(settings_file, "w") as settings:
                 settings.write(setting_file_text)
             return settings_file
@@ -184,8 +366,14 @@ def write_settings_file(project_folder: Path):
 def write_settings_files_orig(base_folder: Path) -> Tuple[int, int]:
 
     # Add a Settings.xml file to each project folder in a base folder.
-    project_folders = [project_folder for project_folder in base_folder.glob("*") if project_folder.is_dir()]
-    count_new_settings_files = sum(write_settings_file(project_folder) for project_folder in project_folders)
-    count_existing_settings_files = len(project_folders) - count_new_settings_files    
+    project_folders = [
+        project_folder
+        for project_folder in base_folder.glob("*")
+        if project_folder.is_dir()
+    ]
+    count_new_settings_files = sum(
+        write_settings_file(project_folder) for project_folder in project_folders
+    )
+    count_existing_settings_files = len(project_folders) - count_new_settings_files
 
     return count_new_settings_files, count_existing_settings_files
